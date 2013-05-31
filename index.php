e157e5a55737ad4df6989f207505703295d1acec
Jared Rosen <jaredarosen@gmail.com>
2013-05-30 21:45:56 -0700

    coming soon

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Jared A Rosen</title>
+
+		<style>
+			* {
+				padding: 0px;
+				margin: 0px;
+			}
+
+			body{
+				background-color: gray;
+				font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+			}
+
+			#container{
+				width: 100%;
+				height: 100%;
+			}
+
+		</style>
+	</head>
+	<body>
+		<div id="container">
+			<img src="https://dl.dropboxusercontent.com/u/23055564/tree.png" alt="tree - coming soon" width="100%"/>
+		</div>
+	</body>
+</html>
