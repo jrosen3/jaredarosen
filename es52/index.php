11407de05ac92e1b21d05179221cce7eca38babb
Jared Rosen <jaredarosen@gmail.com>
2013-12-12 16:45:14 -0500

    ga

--- es52/index.php
+++ es52/index.php
@@ -572,6 +572,16 @@ In addition, a problem with the breadboard implementation of our project was tha
         $("a[rel^='prettyPhoto']").prettyPhoto();
           console.log("premature load");
       </script>
+      <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46410526-1', 'jaredarosen.com');
+  ga('send', 'pageview');
+
+</script>
     </body>
 </html>
 
